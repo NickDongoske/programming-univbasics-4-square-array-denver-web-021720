efb8946505172a4a2e90354feef70ddb3a78ccb0
Nicholas Dongoske <nickdongoske@gmail.com>
2020-01-22 05:33:19 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,9 +2,9 @@ def square_array(array)
   new_numbers = []
   i = 0
   while i < array.length do 
-  # Math.sqrt[i]
+  Math.sqrt[i]
     i += 1
   new_numbers.push()
   end
-  return array
+  return new_numbers
 end
