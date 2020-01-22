c588b2f8b144288bb752e135165d57d001d7cd04
Nicholas Dongoske <nickdongoske@gmail.com>
2020-01-22 05:37:18 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   new_numbers = []
   i = 0
   while i < array.length do 
-  new_numbers << array[i] ** 2
+  new_numbers << array[i].Math.sqrt
   i += 1
   end
   return new_numbers
